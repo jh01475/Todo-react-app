d2eaea44e3e7d006a733f7b1c4d666504e31d1b1
jh0147 <jh0147@hotmail.com>
2023-09-26 10:11:25 +0900

    11. 추가, 수정, 삭제

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Todo from "./Todo";
-import { Paper, List } from "@material-ui/core";
+import AddTodo from "./AddTodo";
+import { Paper, List, Container } from "@material-ui/core";
 import "./App.css";
 class App extends React.Component {
   constructor(props) {
@@ -14,6 +15,22 @@ class App extends React.Component {
       ],
     };
   }
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length; //key를 위한 id 추가
+    item.done = false;
+    thisItems.push(item);
+    this.setState({ items: thisItems }); //update state
+    console.log("items:", this.state.items);
+  };
+  delete = (item) => {
+    const thisItems = this.state.items;
+    const newItems = thisItems.filter((e) => e.id !== item.id);
+    this.setState({ items: newItems }, () => {
+      //디버깅 콜백
+      console.log("Update Items : ", this.state.items);
+    });
+  };
   render() {
     // todoItems에 this.state.items.length 가 0보다 크다면 true 이므로 && 뒤에 값을 넘겨준다.
     // totoItem = this.state.items.length > 0 ? (<Paper></Paper>):""; 이렇게 해도 같은 결과이다. 조건선택문 ? ternary operator
@@ -21,13 +38,20 @@ class App extends React.Component {
       <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} />
+            <Todo item={item} key={item.id} delete={this.delete} />
           ))}{" "}
         </List>
       </Paper>
     );
     // 생성된 컴포넌트 JSX를 리턴한다.
-    return <div className="App">{todoItems}</div>;
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    );
   }
 }
 export default App;
