58ba3e00341d8d3ec13ad0eb1a849aff5d949760
jh0147 <jh0147@hotmail.com>
2023-09-26 09:27:13 +0900

    9

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import Todo from "./Todo";
+import { Paper, List } from "@material-ui/core";
+import "./App.css";
+class App extends React.Component {
+  constructor(props) {
+    //매개변수 props 생성자
+    super(props); // 매개변수 pros 초기화
+    this.state = {
+      // item 에 item.id, item.title, item.done 매개변수 이름과 값 할당
+      items: [
+        { id: 0, title: "Todo 1 ", done: true },
+        { id: 1, title: "Todo 2 ", done: false },
+      ],
+    };
+  }
+  render() {
+    // todoItems에 this.state.items.length 가 0보다 크다면 true 이므로 && 뒤에 값을 넘겨준다.
+    // totoItem = this.state.items.length > 0 ? (<Paper></Paper>):""; 이렇게 해도 같은 결과이다. 조건선택문 ? ternary operator
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} />
+          ))}{" "}
+        </List>
+      </Paper>
+    );
+    // 생성된 컴포넌트 JSX를 리턴한다.
+    return <div className="App">{todoItems}</div>;
+  }
 }
-
 export default App;
---
+++ src/Todo.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { ListItem, ListItemText, InputBase, Checkbox } from "@material-ui/core";
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { item: props.item }; // 매개변수 item 의 변수/값을 item에 대입
+  }
+  render() {
+    const item = this.state.item;
+    return (
+      <ListItem>
+        <Checkbox checked={item.done} />
+        <ListItemText>
+          <InputBase
+            inputProps={{ "aria-label": "naked" }}
+            type="text"
+            id={item.id}
+            name={item.id}
+            value={item.title}
+            multiline={true}
+            fullWidth={true}
+          />
+        </ListItemText>
+      </ListItem>
+    );
+  }
+}
+export default Todo;
